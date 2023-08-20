75cf1bd1c6efd51ff16137d23d7b1e72c13b3bd0
Najrul <mdnajrulislam.4738@gmail.com>
2023-08-20 12:07:41 +0600

    I have provided even handler into every card section and added the total price by adding the item price

--- js/purchase.js
+++ js/purchase.js
@@ -1,6 +1,20 @@
-var totalPrice = 0;
+let totalPrice = 0;
 
 function purchaseItems(data){
-    const purchaseItemPrice = data.childNodes[7].innerText.split(" ")[0]; 
-    
+    const purchaseItemName = data.childNodes[5].innerText;
+    const purchaseItemPriceString = data.childNodes[7].innerText.split(" ")[0]; 
+    const purchaseItemPrice = parseFloat(purchaseItemPriceString) ;
+    totalPrice = totalPrice + purchaseItemPrice;
+    setElementInnerText("total-price", totalPrice);
+    const purchaseItemEntry = document.getElementById('purchase-items-entry')
+    const count = purchaseItemEntry.childElementCount;
+    const p = document.createElement('p');
+    p.innerText = `${count + 1}. ${purchaseItemName}` ;
+    purchaseItemEntry.appendChild(p);
+}
+
+
+function setElementInnerText(elementId, price){
+    const element = document.getElementById(elementId);
+    element.innerText = price.toFixed(2);
 }

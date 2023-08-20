df4616fbac6121edeaf15450f27db611816a8d68
Najrul <mdnajrulislam.4738@gmail.com>
2023-08-20 13:23:17 +0600

    I have calculated and set the discount and total price. Set them in their section. I have add event handler in go home button and When you click on this button, everything will be reset

--- js/purchase.js
+++ js/purchase.js
@@ -1,17 +1,59 @@
-let totalPrice = 0;
+let originalPrice = 0;
+const makePurchase = document.getElementById('make-purchase');
+const couponApplyButton = document.getElementById('apply-btn')
 
 function purchaseItems(data){
     const purchaseItemName = data.childNodes[5].innerText;
     const purchaseItemPriceString = data.childNodes[7].innerText.split(" ")[0]; 
     const purchaseItemPrice = parseFloat(purchaseItemPriceString) ;
-    totalPrice = totalPrice + purchaseItemPrice;
-    setElementInnerText("total-price", totalPrice);
+    originalPrice = originalPrice + purchaseItemPrice;
+    setElementInnerText("total-price", originalPrice);
     const purchaseItemEntry = document.getElementById('purchase-items-entry')
     const count = purchaseItemEntry.childElementCount;
     const p = document.createElement('p');
     p.innerText = `${count + 1}. ${purchaseItemName}` ;
     purchaseItemEntry.appendChild(p);
+    if (originalPrice > 0){
+        makePurchase.removeAttribute('disabled')
+    }
+    if (originalPrice >= 200){
+        couponApplyButton.removeAttribute('disabled')
+    }
 }
+
+couponApplyButton.addEventListener('click', function(){
+    const couponInputField = document.getElementById('coupon-field');
+    const couponInputFieldValue = couponInputField.value;
+    const discountPercentage = 0.2; // 20%
+    const discount = originalPrice * discountPercentage;
+    const totalPrice = originalPrice - discount;
+
+    if (couponInputFieldValue === "SELL200") {
+        setElementInnerText('discount', discount);
+        setElementInnerText('total-price-excludes-discount', totalPrice);
+    } else {
+        setElementInnerText('discount', 0.00);
+        setElementInnerText('total-price-excludes-discount', originalPrice);
+    }
+    couponInputField.value = "";
+})
+
+document.getElementById('back-to-home').addEventListener('click', function(){
+    const purchaseItemsEntry = document.getElementById('purchase-items-entry');
+    purchaseItemsEntry.innerHTML = "";
+    setElementInnerText("total-price", 0.00);
+    setElementInnerText('discount', 0.00);
+    setElementInnerText('total-price-excludes-discount', 0.00);
+
+})
+
+document.getElementById('get-coupon-btn').addEventListener('click', function(){
+    const couponCode = this.innerText;
+    const couponInputField = document.getElementById('coupon-field');
+    if (originalPrice >= 200){
+        couponInputField.value = couponCode;
+    }
+})
 
 
 function setElementInnerText(elementId, price){
